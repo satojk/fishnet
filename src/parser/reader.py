cb75626ccfc7f3a7330cb01e15f1d1379e53fb79
csciutto <cristobal.sciutto@gmail.com>
2018-11-17 16:06:23 -0500

    organize

--- src/parser/reader.py
+++ src/parser/reader.py
@@ -6,13 +6,13 @@ import collections
 
 
 class Game:
-    def __init__(self, pgn_text) :
+    def __init__(self, pgn_text):
         self.pgn_text = pgn_text
         self.moves = [m for m in chess.pgn.read_game(io.StringIO(self.pgn_text)).main_line()]
         self.board = chess.Board()
 
 
-    def ai_player(self) :
+    def ai_player(self):
         lines = self.pgn_text.split('\n')
         if lines[4] == "[White \"lichess AI level 5\"]":
             return 0
@@ -20,46 +20,46 @@ class Game:
             return 1
         raise Exception("No AI players detected")
 
-    def move_num(self) :
+    def move_num(self):
         return len(self.board.move_stack)
     
-    def go_to_move(self, move) :
-        if self.move_num() == move :
+    def go_to_move(self, move):
+        if self.move_num() == move:
             return
-        elif move < 0 or move > len(self.moves) :
+        elif move < 0 or move > len(self.moves):
             raise Exception("move out of bounds in go_to_move")
-        elif self.move_num() < move :
-            for i in range(self.move_num(), move) :
+        elif self.move_num() < move:
+            for i in range(self.move_num(), move):
                 self.board.push(self.moves[i])
-        else :
-            for _ in range(self.move_num() - move) :
+        else:
+            for _ in range(self.move_num() - move):
                 self.board.pop()
  
-    def num_pieces(self) :
+    def num_pieces(self):
         pieces = self.board.piece_map()
         white_num = 0
-        for location, piece in pieces.items() :
-            if piece.color == chess.WHITE :
+        for location, piece in pieces.items():
+            if piece.color == chess.WHITE:
                 white_num += 1
         return (white_num, len(pieces.keys()) - white_num)
 
-    def pieces_lost(self) :
+    def pieces_lost(self):
         self.go_to_move(0)
         captures = []
         pieces = collections.defaultdict(int)
-        for location, piece in self.board.piece_map().items() :
+        for location, piece in self.board.piece_map().items():
             pieces[piece] += 1
-        for i in range(len(self.moves)) :
-            if self.board.is_capture(self.moves[i]) :
+        for i in range(len(self.moves)):
+            if self.board.is_capture(self.moves[i]):
                 self.board.push(self.moves[i])
                 new_pieces = collections.defaultdict(int)
-                for location, piece in self.board.piece_map().items() :
+                for location, piece in self.board.piece_map().items():
                     new_pieces[piece] += 1
-                for piece, quantity in pieces.items() :
-                    if new_pieces[piece] < quantity :
+                for piece, quantity in pieces.items():
+                    if new_pieces[piece] < quantity:
                         captures.append(piece.symbol())
                 pieces = new_pieces
-            else :
+            else:
                 self.board.push(self.moves[i])
         return captures
 
