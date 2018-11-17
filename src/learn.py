b585a928992ded20de565fb1dcd93df45c50d3b1
satojk <lucasjks@gmail.com>
2018-11-17 11:57:16 -0800

    Create simple model
    log-reg with how many pieces each side has at the end

--- src/learn.py
+++ src/learn.py
@@ -1,7 +1,47 @@
+import numpy as np
+from time import sleep
 from parser.reader import Game
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
-with open('./data/samples/example_game.pgn', 'r') as f:
-    data = f.read()
+_PGN_PATH_AND_FILENAME = 'data/games.pgn'
 
-g = Game(data)
-print(g.aiPlayer())
+def load_games():
+    with open(_PGN_PATH_AND_FILENAME, 'r') as f:
+        large_pgn = f.read()
+    pgns = large_pgn.split('\n\n\n')[:-1]
+    return [Game(pgn) for pgn in pgns]
+
+
+def extract_features(games):
+    out = [[],[]]
+    for game in games:
+        game.goToTurn(len(game.moves))
+        out[0].append(game.numPieces())
+        out[1].append(game.aiPlayer())
+    return tuple(out)
+
+
+def dataset_split(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.4, random_state=1)
+    X_test, X_val, y_test, y_val = train_test_split(
+            X_test, y_test, test_size=0.5, random_state=1)
+    return X_train, X_test, X_val, y_train, y_test, y_val
+
+
+def main():
+    games = load_games()
+    X, y = extract_features(games)
+    X = np.array(X)
+    y = np.array(y)
+    X_train, X_test, X_val, y_train, y_test, y_val = dataset_split(X, y)
+    model = LogisticRegression().fit(X_train, y_train)
+    print(model.score(X_test, y_test))
+
+
+if __name__ == '__main__':
+    main()
+
+# g = Game(data)
+# print(g.aiPlayer())
