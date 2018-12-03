bd515517314fc26987dfe81be50773949a763ffc
csciutto <cristobal.sciutto@gmail.com>
2018-12-02 18:56:20 -0800

    wip

--- scraper/pgn_cleaner.py
+++ scraper/pgn_cleaner.py
@@ -1,9 +1,17 @@
-clean = open("../data/fics_games_2017.pgn", "w+")
+# clean = open("../data/fics_2017_HvC.pgn", "w+")
 
-with open("../data/fics_games_2017_raw.pgn") as raw:
-    for i, line in enumerate(raw):
-        if (i % 1000): print(i)
-        if "\r\n" in line:
-            clean.write(line.replace("\r\n", "\n"))
-        else:
-            clean.write("\n\n")
+# with open("../data/fics_2017_HvC_raw.pgn") as raw:
+#     for i, line in enumerate(raw):
+#         if (i % 1000): print(i)
+#         if "\r\n" in line:
+#             clean.write(line.replace("\r\n", "\n"))
+#         else:
+#             clean.write("\n\n")
+#     clean.write("\n\n")
+
+def writeEOF(src):
+    with open(src, "a") as f:
+        f.write("EOF\n")
+
+writeEOF("../data/fics_2017_HvC.pgn")
+writeEOF("../data/games.pgn")
